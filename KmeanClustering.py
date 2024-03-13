afdcc98beddd59ed4c111f3c984d3c6513d9ebac
gavinhsiao <99697676+gavinhsiao@users.noreply.github.com>
2024-03-13 08:30:47 +0800

    first commit

--- KmeanClustering.py
+++ KmeanClustering.py
@@ -17,7 +17,7 @@ from sklearn.preprocessing import StandardScaler
 #import customer data 
 cust_df = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%204/data/Cust_Segmentation.csv")
 cust_df.head()
-#remove unusable column
+#remove unusable column  
 df = cust_df.drop('Address', axis=1)
 df.head()
 
@@ -29,11 +29,10 @@ Clus_dataSet = StandardScaler().fit_transform(X)
 #Clus_dataSet
 
 #set K=3
-clusterNum = 3
+clusterNum = 4
 k_means = KMeans(init = "k-means++", n_clusters = clusterNum, n_init = 12)
 k_means.fit(X)
 labels = k_means.labels_
-print(labels)
 
 df["Clus_km"] = labels
 #df.head(5)
@@ -43,20 +42,19 @@ area = np.pi * ( X[:, 1])**2
 plt.scatter(X[:, 0], X[:, 3], s=area, c=labels.astype(np.float), alpha=0.5)
 plt.xlabel('Age', fontsize=18)
 plt.ylabel('Income', fontsize=16)
-plt.show()
+plt.show(block=False)
+plt.waitforbuttonpress(10)
+
 
 from mpl_toolkits.mplot3d import Axes3D 
 fig = plt.figure(1, figsize=(8, 6))
 plt.clf()
 ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-
+#reset the axes for 3D plotting
 plt.cla()
-# plt.ylabel('Age', fontsize=18)
-# plt.xlabel('Income', fontsize=16)
-# plt.zlabel('Education', fontsize=16)
 ax.set_xlabel('Education')
 ax.set_ylabel('Age')
 ax.set_zlabel('Income')
-
 ax.scatter(X[:, 1], X[:, 0], X[:, 3], c= labels.astype(np.float))
-plt.pause(120)
+plt.waitforbuttonpress(120)
+plt.close()
